547610aa1a03e1aae4ab2b0737fcd6bed9b3f690
Paddy <padampaddy@gmail.com>
2019-12-10 17:32:02 +0530

    changes

--- www/CCAvenuePlugin.js
+++ www/CCAvenuePlugin.js
@@ -1,7 +1,7 @@
 var exec = require('cordova/exec');
 
 exports.showPaymentView = function (success, error, txnParams) {
-    exec(success, error, 'PayUmoneyPnP', 'showPaymentView', [txnParams]);
+    exec(success, error, 'CCAvenueCordova', 'showPaymentView', [txnParams]);
 };
 
 
